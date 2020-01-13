5828cc1c9095bd29f356238b4b4cbb28d65b5e26
鷲見和希 <cs5200@icloud.com>
2020-01-13 15:49:03 +0900

    fix

--- wordpress/backupmysql.sh
+++ wordpress/backupmysql.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # バックアップの保存期間(days)
-period=3
+PERIOD=3
 
 # バックアップ保存用ディレクトリの指定
-dirpath='/mysql/backup'
+DIRPATH='/mysql/backup'
 
 # ファイル名を指定する(※ファイル名で日付がわかるようにしておきます)
-filename=`date +%y%m%d`
+FILENAME=`date +%y%m%d`
 
 # 指定したDBのスキーマおよびデータをすべて吐き出す
-mysqldump -u root --password=password -h localhost wordpress > $dirpath/$filename.sql
+mysqldump -u root --password=password -h localhost wordpress > $DIRPATH/$FILENAME.sql
 
 # パーミッション変更
-sudo chmod 700 $dirpath/$filename.sql
+sudo chmod 700 $DIRPATH/$FILENAME.sql
 
 # 保存期間を過ぎたバックアップを削除
-oldfile=`date --d "$period days ago" +%y%m%d`
-sudo rm -f $dirpath/$oldfile.sql
+OLDFILE=`date --d "$PERIOD days ago" +%y%m%d`
+sudo rm -f $DIRPATH/$OLDFILE.sql
