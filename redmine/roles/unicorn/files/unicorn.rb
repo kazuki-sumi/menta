86425c7d3d41ad0422bec5d863cb1285728a3070
鷲見和希 <cs5200@icloud.com>
2020-02-17 21:12:49 +0900

    unicornタスク作成

---
+++ redmine/roles/unicorn/files/unicorn.rb
@@ -0,0 +1,40 @@
+# /opt/redmine/config/unicorn.rb
+# Unicornは複数のワーカーで起動するのでワーカー数を定義
+# サーバーのメモリなどによって変更すること。
+worker_processes 2
+app_path = "/opt/redmine"
+# Nginxで使用する場合は以下の設定を行う。
+
+listen "#{app_path}/tmp/unicorn_redmine.sock"
+# プロセスの停止などに必要なPIDファイルの保存先を指定。
+pid "#{app_path}/tmp/unicorn.pid"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+# Unicornのエラーログと通常ログの位置を指定。
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+preload_app true
+# 接続タイムアウト時間
+timeout 30
+
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
